7ac7239b2782b81f6d10402868526589ae903923
Maksymilian Boguń <maksymilian.bogun@doverfs.com>
2021-04-28 11:07:42 +0200

    update providers.base.json

--- schemas-extractor/fetch-providers.sh
+++ schemas-extractor/fetch-providers.sh
@@ -5,10 +5,21 @@ IFS=$'\n\t'
 
 pushd "$(dirname "$0")" >/dev/null
 
-get_repos() {
+get_org_repos() {
   echo "Fetching repositories from organization $1..."
   local idx=1
-  while curl -s "https://api.github.com/orgs/$1/repos?sort=full_name&per_page=100&page=${idx}" | jq -re '.[].name' >>"$2"; do
+  while curl -u maxbog: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 -s "https://api.github.com/orgs/$1/repos?sort=full_name&per_page=100&page=${idx}" | jq -re '.[].name' >>"$2"; do
+    echo "Fetched page $idx"
+    idx=$((idx + 1))
+  done
+  echo "Done"
+}
+
+
+get_user_repos() {
+  echo "Fetching repositories from user $1..."
+  local idx=1
+  while curl -u maxbog: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 -s "https://api.github.com/users/$1/repos?sort=full_name&per_page=100&page=${idx}" | jq -re '.[].name' >>"$2"; do
     echo "Fetched page $idx"
     idx=$((idx + 1))
   done
@@ -16,8 +27,9 @@ get_repos() {
 }
 
 rm -f repositories.list.full
-get_repos "terraform-providers" repositories.list.full
-get_repos "hashicorp" repositories.list.full
+get_org_repos "terraform-providers" repositories.list.full
+get_org_repos "hashicorp" repositories.list.full
+get_user_repos "Mongey" repositories.list.full
 echo "Filtering terrform providers repos..."
 grep -- '^terraform-provider-' repositories.list.full | awk '{print substr($0, 20)}' | sort >providers.list
 
